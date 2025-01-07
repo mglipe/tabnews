f182a80c6c2a4dc7c4cd03c114989a99aa987363
mglipe <marcelogarcia000@hotmial.com>
2025-01-07 14:49:11 -0300

    finalizando endpoint status

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,7 +1,32 @@
 import database from "infra/database.js";
 
 export default async function status(request, response) {
-  const result = await database.query("select 1 + 1;");
-  console.log(result);
-  return response.json({ status: "status here" });
+  const updateAt = new Date();
+  //versão postgres
+  const databaseVersion = await database.query("SHOW server_version;");
+  const databaseVersionValue = parseInt(databaseVersion.rows[0].server_version);
+  //conexão máxima
+  const maxConnection = await database.query("SHOW max_connections;");
+  const maxConnectionValue = parseInt(maxConnection.rows[0].max_connections);
+
+  const datname = process.env.POSTGRES_DB;
+  //conexão usada
+  const activeConnection = await database.query({
+    text: `SELECT COUNT(*)::int FROM pg_stat_activity WHERE datname = $1;`,
+    values: [datname],
+  });
+
+  const activeConnectionValue = activeConnection.rows[0].count;
+
+  return response.json({
+    update_at: updateAt,
+    dependencies: {
+      database: {
+        active_connection: activeConnectionValue,
+        max_connections: maxConnectionValue,
+        version: databaseVersionValue,
+      },
+      webservice: {},
+    },
+  });
 }
--- test/api/v1/status/get.test.js
+++ test/api/v1/status/get.test.js
@@ -1,4 +1,14 @@
 test("running endpoint status", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+  const reqBody = await response.json();
+
+  const databaseVersion = reqBody.dependencies.database.version;
+  expect(databaseVersion).toEqual(16.0);
+
+  const maxConnection = reqBody.dependencies.database.max_connections;
+  expect(maxConnection).toEqual(100);
+
+  const activeConnection = reqBody.dependencies.database.active_connection;
+  expect(activeConnection).toEqual(1);
 });
