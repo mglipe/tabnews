65465ad6bf5828cc0e5f6d6b8134ae4ea8b9c223
mglipe <marcelogarcia000@hotmial.com>
2025-02-03 16:30:17 -0300

    add statuspage

--- pages/statusPage/index.js
+++ pages/statusPage/index.js
@@ -6,10 +6,33 @@ async function fetchApi(key) {
   return responseBody;
 }
 
+function VersionDb() {
+  const { isLoading, data } = useSWR("/api/v1/status", fetchApi);
+
+  let dataDatabase = "loading...";
+
+  if (!isLoading && data) {
+    dataDatabase = (
+      <>
+        <div>Conexão Ativa: {data.dependencies.database.active_connection}</div>
+        <div>Máximo Conexões: {data.dependencies.database.max_connections}</div>
+        <div>Versão: {data.dependencies.database.version}</div>
+      </>
+    );
+  }
+
+  return (
+    <>
+      <p>{dataDatabase}</p>
+    </>
+  );
+}
+
 function UpdateAt() {
   const { isLoading, data } = useSWR("/api/v1/status", fetchApi, {
     refreshInterval: 2000,
   });
+  console.log("pint: ", data);
   let updateAtText = "loading...";
   if (!isLoading && data) {
     updateAtText = new Date(data.update_at).toString("pt-BR");
@@ -22,6 +45,8 @@ export default function statusPage() {
     <>
       <h1>Status</h1>
       <UpdateAt />
+      <h1>Database</h1>
+      <VersionDb />
     </>
   );
 }
